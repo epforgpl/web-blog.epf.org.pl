96cf104c3b7ce9e9163d80979fc9638933306022
CainIsh <mariusz.machuta@gmail.com>
2015-01-14 14:19:29 +0100

    clean up + zmiany

--- loop.php
+++ loop.php
@@ -1,21 +1,21 @@
 <section>
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
+            <h2>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+            </h2>
 
-		<?php the_content(); ?>
+            <?php the_content(); ?>
 
-		<?php edit_post_link(); ?>
+            <?php edit_post_link(); ?>
 
-	</article>
-<?php endwhile; ?>
+        </article>
+    <?php endwhile; ?>
 
-<?php else: ?>
-	<article>
-		<h2>Sorry, nothing to display.</h2>
-	</article>
-<?php endif; ?>
+    <?php else: ?>
+        <article>
+            <h2>Sorry, nothing to display.</h2>
+        </article>
+    <?php endif; ?>
 </section>
